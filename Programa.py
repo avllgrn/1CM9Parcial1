ab38c8aa81b651697840720baf2a30ec2eb536b5
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-26 17:36:48 -0600

    Grados a Radianes, Radianes a Grados

--- Programa.py
+++ Programa.py
@@ -1,48 +1,19 @@
 import os
 import math
 
-def formulaGeneralX1(a, b, c):
-    discr = math.pow(b, 2) - 4*a*c
-    if a==0:
-        print('Error! Raíces indeterminadas...')
-        return None
-    elif discr<0:
-        print('Error! Raíces imaginarias...')
-        return None
-    else:
-        x1 = (-b+math.sqrt(discr)) / (2*a)
-        return x1
+def convierteGradosARadianes(grados):
+    return grados*math.pi/180
 
-def formulaGeneralX2(a, b, c):
-    discr = math.pow(b, 2) - 4*a*c
-    if a==0:
-        print('Error! Raíces indeterminadas...')
-        return None
-    elif discr<0:
-        print('Error! Raíces imaginarias...')
-        return None
-    else:
-        x2 = (-b-math.sqrt(discr)) / (2*a)
-        return x2
-
+def convierteRadianesAGrados(radianes):
+    return radianes*180/math.pi
 
 if __name__ == '__main__':
     os.system('cls')
 
-    a = float(input('Ingresa a '))
-    b = float(input('Ingresa b '))
-    c = float(input('Ingresa c '))
-    discr = math.pow(b, 2) - 4*a*c
+    xGrados = float(input('Ingresa grados '))    
+    xRadianes = convierteGradosARadianes(xGrados)
+    print(f'{xGrados}° = {xRadianes} Rads')
 
-    if a==0:
-        print('Error! Raíces indeterminadas...')
-    elif discr<0:
-        print('Error! Raíces imaginarias...')
-    else:
-        x1 = formulaGeneralX1(a,b,c)
-        x2 = formulaGeneralX2(a,b,c)
-        print(f'a = {a}')
-        print(f'b = {b}')
-        print(f'c = {c}')
-        print(f'x1 = {x1}')
-        print(f'x2 = {x2}')
+    xRadianes = float(input('Ingresa radianes '))
+    xGrados = convierteRadianesAGrados(xRadianes)
+    print(f'{xRadianes}° = {xGrados} Rads')
