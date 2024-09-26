a59e2a1e0a063cda5f1da5b6cd625185ab02e1b1
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-26 17:14:19 -0600

    Suma de dos numeros

--- Programa.py
+++ Programa.py
@@ -1,1 +1,13 @@
-print('Hola, mundo!')
+import os
+
+def suma(x, y):
+    z = x+y
+    return z
+
+if __name__ == '__main__':
+    os.system('cls')
+    
+    a = int(input('Ingresa un entero '))
+    b = int(input('Ingresa otro entero '))
+    c = suma(a, b)
+    print(f'{a} + {b} = {c}')
