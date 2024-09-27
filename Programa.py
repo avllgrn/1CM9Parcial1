79feb22a2e581f3c2cbb8d4ec0ee601937185e3a
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-27 09:41:06 -0600

    Tablas de seno, coseno y tangente

--- Programa.py
+++ Programa.py
@@ -1,21 +1,44 @@
 import os
 import math
 
-def contadorAscendente(ini, fin, inc):
+def convierteGradosARadianes(grados):
+    return grados*math.pi/180
+
+def seno(x):
+    return math.sin(convierteGradosARadianes(x))
+
+def coseno(x):
+    return math.cos(convierteGradosARadianes(x))
+
+def tangente(x):
+    return math.tan(convierteGradosARadianes(x))
+
+def tablaDelSeno(ini, fin, inc):
     inc = math.fabs(inc)
     if inc>0:
+        print(f'x\t| f(x) = sen(x)\n')
         i = ini
         while i<=fin:
-            print(i)
+            print(f'{i}\t| {seno(i)}')
             i = i+inc
 
-def contadorDescendente(ini, fin, dec):
-    dec = math.fabs(dec)
-    if dec>0:
+def tablaDelCoseno(ini, fin, inc):
+    inc = math.fabs(inc)
+    if inc>0:
+        print(f'x\t| f(x) = cosen(x)\n')
         i = ini
-        while i>=fin:
-            print(i)
-            i = i-dec
+        while i<=fin:
+            print(f'{i}\t| {coseno(i)}')
+            i = i+inc
+
+def tablaDeTangente(ini, fin, inc):
+    inc = math.fabs(inc)
+    if inc>0:
+        print(f'x\t| f(x) = tangente(x)\n')
+        i = ini
+        while i<=fin:
+            print(f'{i}\t| {tangente(i)}')
+            i = i+inc
 
 if __name__ == '__main__':
     os.system('cls')
@@ -24,7 +47,11 @@ if __name__ == '__main__':
     final = float(input('Ingresa final '))
     intervalo = float(input('Ingresa intervalo '))
 
-    if inicio<final:
-        contadorAscendente(inicio, final, intervalo)
-    else:
-        contadorDescendente(inicio, final, intervalo)
+    tablaDelSeno(inicio, final, intervalo)
+    print('\n')
+
+    tablaDelCoseno(inicio, final, intervalo)
+    print('\n')
+
+    tablaDeTangente(inicio, final, intervalo)
+    print('\n')
