a97d38090cf501e37cf113a6fb45b0f03d44ac9c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-27 09:49:47 -0600

    Menu repetitivo

--- Programa.py
+++ Programa.py
@@ -41,17 +41,36 @@ def tablaDeTangente(ini, fin, inc):
             i = i+inc
 
 if __name__ == '__main__':
-    os.system('cls')
 
-    inicio = float(input('Ingresa inicio '))
-    final = float(input('Ingresa final '))
-    intervalo = float(input('Ingresa intervalo '))
+    while True:
+        os.system('cls')
+        print('1. Tabla del seno')
+        print('2. Tabla del coseno')
+        print('3. Tabla de la tangente')
+        print('4. Salir')
+        opcion = input('Ingresa tu opcion ')
+        os.system('cls')
 
-    tablaDelSeno(inicio, final, intervalo)
-    print('\n')
+        match opcion:
+            case '1':
+                inicio = float(input('Ingresa inicio '))
+                final = float(input('Ingresa final '))
+                intervalo = float(input('Ingresa intervalo '))
+                tablaDelSeno(inicio, final, intervalo)
+            case '2':
+                inicio = float(input('Ingresa inicio '))
+                final = float(input('Ingresa final '))
+                intervalo = float(input('Ingresa intervalo '))
+                tablaDelCoseno(inicio, final, intervalo)
+            case '3':
+                inicio = float(input('Ingresa inicio '))
+                final = float(input('Ingresa final '))
+                intervalo = float(input('Ingresa intervalo '))
+                tablaDeTangente(inicio, final, intervalo)
+            case '4':
+                print('Adios!')
+                break
+            case _:
+                print('Opcion invalida!')
 
-    tablaDelCoseno(inicio, final, intervalo)
-    print('\n')
-
-    tablaDeTangente(inicio, final, intervalo)
-    print('\n')
+        os.system('pause')
