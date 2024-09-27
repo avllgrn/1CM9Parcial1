0704ac8d5a9aba9ef2eadac09354356691fac5d2
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-09-27 09:32:30 -0600

    Contadores ascendente y descendente

--- Programa.py
+++ Programa.py
@@ -1,41 +1,30 @@
 import os
 import math
 
-def convierteGradosARadianes(grados):
-    return grados*math.pi/180
+def contadorAscendente(ini, fin, inc):
+    inc = math.fabs(inc)
+    if inc>0:
+        i = ini
+        while i<=fin:
+            print(i)
+            i = i+inc
 
-def convierteRadianesAGrados(radianes):
-    return radianes*180/math.pi
-
-def seno(x):
-    return math.sin(convierteGradosARadianes(x))
-
-def coseno(x):
-    return math.cos(convierteGradosARadianes(x))
-
-def tangente(x):
-    return math.tan(convierteGradosARadianes(x))
+def contadorDescendente(ini, fin, dec):
+    dec = math.fabs(dec)
+    if dec>0:
+        i = ini
+        while i>=fin:
+            print(i)
+            i = i-dec
 
 if __name__ == '__main__':
     os.system('cls')
 
-    print('1. Convertir a radianes')
-    print('2. Seno')
-    print('3. Coseno')
-    print('4. Tangente')
-    opcion = input('Ingresa tu opcion ')
+    inicio = float(input('Ingresa inicio '))
+    final = float(input('Ingresa final '))
+    intervalo = float(input('Ingresa intervalo '))
 
-    match opcion:
-        case '1':
-            grados = float(input('Ingresa grados '))
-            radianes = convierteGradosARadianes(grados)
-            print(f'{grados}° = {radianes} rads')
-        case '2':
-            grados = float(input('Ingresa grados '))
-            print(f'seno({grados}) = {seno(grados)}')
-        case '3':
-            grados = float(input('Ingresa grados '))
-            print(f'coseno({grados}) = {coseno(grados)}')
-        case '4':
-            grados = float(input('Ingresa grados '))
-            print(f'tangente({grados}) = {tangente(grados)}')
+    if inicio<final:
+        contadorAscendente(inicio, final, intervalo)
+    else:
+        contadorDescendente(inicio, final, intervalo)
